a89d1c150ebfd27fa8e2288bc9004c1032c37418
Martin Skalický <skalicky.martas@seznam.cz>
2020-02-07 20:30:02 +0100

    Logger separation

--- src/modules/framework-ui/src/Logger/index.js
+++ src/modules/framework-ui/src/Logger/index.js
@@ -1,33 +1,5 @@
-const styleArray = {
-	red: ['color: red', '\x1b[31m'],
-	green: ['color: green', '\x1b[32m'],
-	yellow: ['color: #dddd36', '\x1b[33m'],
-	orange: ['color: #f57c00'],
-	reset: ['color:unset', '\x1b[0m'],
-	bold: ['color:unset; font-weight: bold;']
-}
+import logger from 'framework/src/Logger/fn'
 
-function logger(useCss = true, styles = styleArray) {
-	let entry;
-	return function configLogger(logger, logMethod = 'log') {
-		const log = logger[logMethod];
-		return function loggerColor(color, message = '') {
-			const style = styles[color];
-			return function (...args) {
-				// create entry message (true = browser / false = server)
-				if (useCss) 
-					entry = [`%c${message}`, style[0]];
-				 else {
-					entry = [`${style[1]}${message}${styles['reset'][1]}`];
-				 }
-				 				
-				// log message
-				log.apply(logger, [...entry, ...args]); // logger: Console
-				return args
-			}
-		}
-	}
-}
 const loggerCSS = logger(true);
 
  const consoleLog = loggerCSS(console);
@@ -36,7 +8,4 @@ const loggerCSS = logger(true);
 export const infoLog = consoleLog('green', 'INFO');
 export const warningLog = consoleLog('orange', 'WARNING');
 export const errorLog = consoleLog('red', 'ERROR');
-
-// infoLog({id: 13, user: 'martin'});
-// warningLog({id: 13, user: 'martin'});
-// errorLog({id: 13, user: 'martin'});
+export const devLog = consoleLog('blue', 'DEV', true);
--- src/modules/framework/src/Logger/index.js
+++ src/modules/framework/src/Logger/index.js
@@ -1,33 +1,5 @@
-const styleArray = {
-     red: ['color: red', '\x1b[31m'],
-     green: ['color: green', '\x1b[32m'],
-     yellow: ['color: #dddd36', '\x1b[33m'],
-     blue: ['color lightblue', '\x1b[34m'],
-     reset: ['color:unset', '\x1b[0m']
-};
+import logger from './fn'
 
-function logger(useCss = true, styles = styleArray) {
-     let entry;
-     return function configLogger(logger, logMethod = 'log') {
-          const log = logger[logMethod];
-          return function loggerColor(color, message = '', devOnly = false) {
-               const style = styles[color];
-               return function (...value) {
-                    // create entry message (true = browser / false = server)
-                    if (useCss) entry = [`%c${message}`, style[0]];
-                    else {
-                         entry = [`${style[1]}${message}${styles['reset'][1]}`];
-                    }
-
-                    // log message
-                    if ((devOnly && process.env.NODE_ENV === 'development') || !devOnly)
-                         log.apply(logger, [...entry, ...value]); // logger: Console
-
-                    return value;
-               };
-          };
-     };
-}
 const baseLogger = logger(false);
 
 const consoleLog = baseLogger(console);
@@ -35,8 +7,5 @@ const consoleLog = baseLogger(console);
 export const infoLog = consoleLog('green', 'INFO');
 export const warningLog = consoleLog('yellow', 'WARNING');
 export const errorLog = consoleLog('red', 'ERROR');
-export const devLog = consoleLog('blue', 'LOG', true);
+export const devLog = consoleLog('blue', 'DEV', true);
 
-// infoLog({id: 13, user: 'martin'});
-// warningLog({id: 13, user: 'martin'});
-// errorLog({id: 13, user: 'martin'});
