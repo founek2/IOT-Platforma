c3ba2e687c2aac749a225d37f98af0dabc69144a
Martin Skalický <skalicky.martin@iotplatforma.cloud>
2020-02-19 16:07:14 +0100

    Add proxy for images

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,5 +5,6 @@ module.exports = function(app) {
   app.use(proxy('/api', { target: 'http://localhost:8085' }));
   app.use(proxy('/socket.io', { target: 'http://localhost:8085/socket.io', ws: true }));
   // app.use(proxy('/api', { target: 'https://test.iotplatforma.cloud', secure: false, changeOrigin: true }));
-  // app.use(proxy('/socket.io', { target: 'https://test.iotplatforma.cloud/socket.io', ws: true, secure: false, changeOrigin: true }));
+  // app.use(proxy('/socket.io', { target: 'https://test.iotplatforma.cloud', ws: true, secure: false, changeOrigin: true }));
+  // app.use(proxy('/images', { target: 'https://test.iotplatforma.cloud', secure: false, changeOrigin: true }));
 };
