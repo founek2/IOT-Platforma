f6d35a2761e792328ff97419b2a5cf245f072b34
Martin Skalický <skalicky.martin@iotplatforma.cloud>
2020-03-15 19:42:10 +0100

    Enable PWA register

--- src/modules/frontend/src/index.js
+++ src/modules/frontend/src/index.js
@@ -12,4 +12,4 @@ ReactDOM.render(<Root component={Router} />, document.getElementById('root'))
 // Learn more about service workers: http://bit.ly/CRA-PWA
 // if (process.env.NODE_ENV === 'production') serviceWorker.register()
 // else serviceWorker.unregister()
-serviceWorker.unregister()
+serviceWorker.register()
