78df6f9684504287ed7e4b7ab5e3c8e9f2f159c9
Martin Skalický <skalicky.martin@iotplatforma.cloud>
2020-04-12 18:06:01 +0200

    FIX websocket multiple connection

--- src/modules/frontend/src/webSocket/index.js
+++ src/modules/frontend/src/webSocket/index.js
@@ -16,6 +16,13 @@ class MySocket {
         })
         newCon.open()
         mySocket.applyListeners(newCon)
+        // newCon.on("connect", function() {
+        //     console.log("connected")
+
+        //     newCon.on("disconnect", function() {
+        //         console.log("DC")
+        //     })
+        // })
         this.socket = newCon;
     }
 
@@ -35,7 +42,7 @@ class MySocket {
     }
 
     close = () => {
-        if (this.socket.connected)
+        // if (this.socket.connected)
             this.socket.close()
     }
 }
