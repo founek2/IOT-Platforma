13f5b5b3dec96260c408a78ff1dac8fd73ad5a8b
Martin Skalický <skalicky.martin@iotplatforma.cloud>
2020-06-07 13:56:04 +0200

    Firebase prepation

--- src/modules/frontend/src/sw-build.js
+++ src/modules/frontend/src/sw-build.js
@@ -3,7 +3,7 @@ const workboxBuild = require("workbox-build");
 const buildSW = () => {
     return workboxBuild.injectManifest({
         swSrc: "src/modules/frontend/src/sw.js", // custom sw rule
-        swDest: "build/sw.js", // sw output file (auto-generated
+        swDest: "build/firebase-messaging-sw.js", // sw output file (auto-generated
         globDirectory: "build",
         globPatterns: ["**/*.{js,css,html,svg}"],
         maximumFileSizeToCacheInBytes: 3 * 1024 * 1024,
--- src/modules/frontend/src/sw.js
+++ src/modules/frontend/src/sw.js
@@ -2,6 +2,8 @@
 if ("function" === typeof importScripts) {
 
   importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+  importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-app.js');
+  importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-messaging.js');
 
   // Global workbox
   if (workbox) {
@@ -79,6 +81,30 @@ if ("function" === typeof importScripts) {
         ]
       })
     );
+
+    firebase.initializeApp({
+      apiKey: "AIzaSyD15CUfT9PGQNok0qcs4qu2ectuauAYwWw",
+      authDomain: "iot-platforma-v2.firebaseapp.com",
+      databaseURL: "https://iot-platforma-v2.firebaseio.com",
+      projectId: "iot-platforma-v2",
+      storageBucket: "iot-platforma-v2.appspot.com",
+      messagingSenderId: "2726593509",
+      appId: "1:2726593509:web:67c7b307fdf98f6973a50c"
+    })
+
+    var messaging = firebase.messaging();
+    messaging.setBackgroundMessageHandler(function (payload) {
+      console.log('[firebase-messaging-sw.js] Received background message ', payload);
+      // Customize notification here
+      const notificationTitle = 'Background Message Title';
+      const notificationOptions = {
+        body: 'Background Message body.',
+        icon: '/firebase-logo.png'
+      };
+
+      return self.registration.showNotification(notificationTitle,
+        notificationOptions);
+    });
 
   } else {
     console.error("Workbox could not be loaded. No offline support");
