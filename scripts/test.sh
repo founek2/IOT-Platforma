c3a7683d8b4f323c564b4572592fd820e7e06056
Martin Skalický <skalicky.martin@iotplatforma.cloud>
2020-06-28 10:54:27 +0200

    Add grep argument for test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,6 +4,25 @@
 # -e when any command fails, the script emmediately exit
 set -u
 
+# parse_params() {
+# A POSIX variable
+OPTIND=1 # Reset in case getopts has been used previously in the shell.
+
+# Initialize our own variables:
+GREP_PARAM=""
+while getopts "g:" opt; do
+    case "$opt" in
+    g)
+        GREP_PARAM=$OPTARG
+        ;;
+    esac
+done
+
+shift $((OPTIND - 1))
+
+[ "${1:-}" = "--" ] && shift
+# echo "ignoring params: $@"
+
 SCRIPTPATH="$(
     cd "$(dirname "$0")" >/dev/null 2>&1
     pwd -P
@@ -64,7 +83,7 @@ all)
     run_server
 
     check_running
-    yarn mocha \
+    yarn mocha -g "$GREP_PARAM" \
         "$BE_TEST_PATH" \
         "$BE_MQTT_TEST_PATH" \
         "$FRAMEWORK_UI_TEST_PATH"
@@ -72,13 +91,13 @@ all)
     stop_server
     ;;
 FE)
-    yarn mocha "$FE_TEST_PATH"
+    yarn mocha -g "$GREP_PARAM" "$FE_TEST_PATH"
     ;;
 BE)
     # run_server
 
     check_running
-    yarn mocha "$BE_TEST_PATH"
+    yarn mocha -g "$GREP_PARAM" "$BE_TEST_PATH"
 
     # stop_server
     ;;
@@ -86,15 +105,15 @@ BE-mqtt)
     run_server
 
     check_running
-    yarn mocha "$BE_MQTT_TEST_PATH"
+    yarn mocha -g "$GREP_PARAM" "$BE_MQTT_TEST_PATH"
 
     stop_server
     ;;
 f)
-    yarn mocha "$FRAMEWORK_TEST_PATH"
+    yarn mocha -g "$GREP_PARAM" "$FRAMEWORK_TEST_PATH"
     ;;
 f-ui)
-    yarn mocha "$FRAMEWORK_UI_TEST_PATH"
+    yarn mocha -g "$GREP_PARAM" "$FRAMEWORK_UI_TEST_PATH"
     ;;
 *)
     echo "Usage: {all|all-results|BE|BE-mqtt|FE|f|f-ui|runBE|stopBE}" >&2
