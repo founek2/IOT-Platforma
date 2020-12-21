98198bdedc7f3b3981ee37271550c99e3d069904
Martin Skalický <skalicky.martin@iotplatforma.cloud>
2020-12-21 17:33:51 +0100

    Remove ID from Select

--- src/modules/framework-ui/src/Components/Select.js
+++ src/modules/framework-ui/src/Components/Select.js
@@ -8,29 +8,29 @@ import Input from '@material-ui/core/Input';
 import styles from './fieldConnector/styles'
 
 function OwnSelect(props) {
-     const { label, value, onChange, onBlur, onFocus, name, autoFocus, selectOptions, FormHelperTextProps, helperText, required, error, className, classes, ...other } = props;
-     return (
-          <FormControl className={`${classes.textField} ${className || ""}`}>
-               {label && <InputLabel htmlFor={`select-label-placeholder-${name}`}>{label}</InputLabel>}
-               <Select
-                    onChange={onChange}
-                    value={value}
-                    error={error}
-                    required={required}
-                    onFocus={onFocus}
-                    onBlur={onBlur}
-                    name={name}
-                    autoFocus={autoFocus}
-                    input={<Input name={name} id={`select-label-placeholder-${name}`} />}
-                    {...other}
-               >
-                    {selectOptions}
-               </Select>
-               {error && <FormHelperText {...FormHelperTextProps}>{helperText}</FormHelperText>}
-          </FormControl>
-     );
+    const { label, value, onChange, onBlur, onFocus, name, autoFocus, selectOptions, FormHelperTextProps, helperText, required, error, className, classes, ...other } = props;
+    return (
+        <FormControl className={`${classes.textField} ${className || ""}`}>
+            {label && <InputLabel htmlFor={`select-label-placeholder-${name}`}>{label}</InputLabel>}
+            <Select
+                onChange={onChange}
+                value={value}
+                error={error}
+                required={required}
+                onFocus={onFocus}
+                onBlur={onBlur}
+                name={name}
+                autoFocus={autoFocus}
+                input={<Input name={name} />}
+                {...other}
+            >
+                {selectOptions}
+            </Select>
+            {error && <FormHelperText {...FormHelperTextProps}>{helperText}</FormHelperText>}
+        </FormControl>
+    );
 }
 OwnSelect.defaultProps = {
-     value: ""
+    value: ""
 }
 export default withStyles(styles)(OwnSelect);
