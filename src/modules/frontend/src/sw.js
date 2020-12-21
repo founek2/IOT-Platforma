a631b53a9dd1870dd4822a3260cfc4287777a2b0
Martin Skalický <skalicky.martin@iotplatforma.cloud>
2020-12-21 16:40:29 +0100

    Add check of messaging support in SW

--- src/modules/frontend/src/sw.js
+++ src/modules/frontend/src/sw.js
@@ -17,19 +17,21 @@ if ("function" === typeof importScripts) {
         measurementId: "G-WEYQG0L8R3"
     })
 
-    var messaging = firebase.messaging();
-    messaging.setBackgroundMessageHandler(function (payload) {
-        console.log('[firebase-messaging-sw.js] Received background message ', payload);
-        // Customize notification here
-        const notificationTitle = 'Background Message Title';
-        const notificationOptions = {
-            body: 'Background Message body.',
-            icon: '/firebase-logo.png'
-        };
+    if (firebase.messaging.isSupported()) {
+        var messaging = firebase.messaging();
+        messaging.setBackgroundMessageHandler(function (payload) {
+            console.log('[firebase-messaging-sw.js] Received background message ', payload);
+            // Customize notification here
+            const notificationTitle = 'Background Message Title';
+            const notificationOptions = {
+                body: 'Background Message body.',
+                icon: '/firebase-logo.png'
+            };
 
-        return self.registration.showNotification(notificationTitle,
-            notificationOptions);
-    });
+            return self.registration.showNotification(notificationTitle,
+                notificationOptions);
+        });
+    }
 
     // Global workbox
     if (workbox) {
