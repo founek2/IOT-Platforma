18d168f4e7b7d3dca33be213600b44fba89f4a9d
Martin Skalický <skalicky.martin@iotplatforma.cloud>
2020-12-21 16:59:39 +0100

    FIX sw

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,29 +1,31 @@
 if ("function" === typeof importScripts) {
-  importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-app.js');
-  importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-messaging.js');
+    importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
+    importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
 
     firebase.initializeApp({
-      apiKey: "AIzaSyD15CUfT9PGQNok0qcs4qu2ectuauAYwWw",
-      authDomain: "iot-platforma-v2.firebaseapp.com",
-      databaseURL: "https://iot-platforma-v2.firebaseio.com",
-      projectId: "iot-platforma-v2",
-      storageBucket: "iot-platforma-v2.appspot.com",
-      messagingSenderId: "2726593509",
-      appId: "1:2726593509:web:67c7b307fdf98f6973a50c"
+        apiKey: "AIzaSyD15CUfT9PGQNok0qcs4qu2ectuauAYwWw",
+        authDomain: "iot-platforma-v2.firebaseapp.com",
+        databaseURL: "https://iot-platforma-v2.firebaseio.com",
+        projectId: "iot-platforma-v2",
+        storageBucket: "iot-platforma-v2.appspot.com",
+        messagingSenderId: "2726593509",
+        appId: "1:2726593509:web:67c7b307fdf98f6973a50c"
     })
 
-    var messaging = firebase.messaging();
-    messaging.setBackgroundMessageHandler(function (payload) {
-      console.log('[firebase-messaging-sw.js] Received background message ', payload);
-      // Customize notification here
-      const notificationTitle = 'Background Message Title';
-      const notificationOptions = {
-        body: 'Background Message body.',
-        icon: '/firebase-logo.png'
-      };
+    if (firebase.messaging.isSupported()) {
+        var messaging = firebase.messaging();
+        messaging.setBackgroundMessageHandler(function (payload) {
+            console.log('[firebase-messaging-sw.js] Received background message ', payload);
+            // Customize notification here
+            const notificationTitle = 'Background Message Title';
+            const notificationOptions = {
+                body: 'Background Message body.',
+                icon: '/firebase-logo.png'
+            };
 
-      return self.registration.showNotification(notificationTitle,
-        notificationOptions);
-    });
+            return self.registration.showNotification(notificationTitle,
+                notificationOptions);
+        });
+    }
 
 }
--- src/modules/frontend/src/sw.js
+++ src/modules/frontend/src/sw.js
@@ -3,36 +3,6 @@ if ("function" === typeof importScripts) {
 
     importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
-    importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
-    importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
-
-    firebase.initializeApp({
-        apiKey: "AIzaSyD15CUfT9PGQNok0qcs4qu2ectuauAYwWw",
-        authDomain: "iot-platforma-v2.firebaseapp.com",
-        databaseURL: "https://iot-platforma-v2.firebaseio.com",
-        projectId: "iot-platforma-v2",
-        storageBucket: "iot-platforma-v2.appspot.com",
-        messagingSenderId: "2726593509",
-        appId: "1:2726593509:web:67c7b307fdf98f6973a50c",
-        measurementId: "G-WEYQG0L8R3"
-    })
-
-    if (firebase.messaging.isSupported()) {
-        var messaging = firebase.messaging();
-        messaging.setBackgroundMessageHandler(function (payload) {
-            console.log('[firebase-messaging-sw.js] Received background message ', payload);
-            // Customize notification here
-            const notificationTitle = 'Background Message Title';
-            const notificationOptions = {
-                body: 'Background Message body.',
-                icon: '/firebase-logo.png'
-            };
-
-            return self.registration.showNotification(notificationTitle,
-                notificationOptions);
-        });
-    }
-
     // Global workbox
     if (workbox) {
 
