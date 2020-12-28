160c72eb62aa5aa36ee09ccac9856b15c5bc4ff8
Martin Skalický <skalima7@fit.cvut.cz>
2020-12-28 20:19:45 +0100

    Add center Prop to Loader

--- packages/framework-ui/src/Components/Loader.js
+++ packages/framework-ui/src/Components/Loader.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import clsx from 'clsx';
 
-if (process.env.BABEL_ENV !== "node") {
+if (process.env.BABEL_ENV !== 'node') {
 	require('./Loader/index.css');
 }
 
-function Loader({open, className}) {
-     return open ? (
-          <div className={`relative ${className ? className : "" }`}>
-               <div className="loader-5">
-                    <span />
-               </div>
-          </div>
-     ) : null;
+function Loader({ open, className, center }) {
+	return open ? (
+		<div className={clsx('relative', className, center && 'loader--center')}>
+			<div className="loader-5">
+				<span />
+			</div>
+		</div>
+	) : null;
 }
 export default Loader;
