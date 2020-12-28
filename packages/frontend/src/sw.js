d48f81334e299104af03d2f064d6379998997d98
Martin Skalický <skalima7@fit.cvut.cz>
2020-12-28 20:38:21 +0100

    Disable force SW install

--- packages/frontend/src/sw.js
+++ packages/frontend/src/sw.js
@@ -1,80 +1,69 @@
 /* eslint-disable */
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 
 if (workbox) {
+	console.log('Workbox is loaded');
 
-    console.log("Workbox is loaded");
+	// Disable logging
+	workbox.setConfig({ debug: false });
 
-    // Disable logging
-    workbox.setConfig({ debug: false });
+	self.addEventListener('message', function(event) {
+		if (event.data.action === 'skipWaiting') {
+			console.log('Skipping waiting');
+			self.skipWaiting();
+		}
+	});
 
+	// The event listener that is fired when the service worker updates
+	// Here we reload the page
+	// navigator.serviceWorker.addEventListener('controllerchange', function () {
 
-    //`generateSW` and `generateSWString` provide the option 
-    // to force update an exiting service worker.
-    // Since we're using `injectManifest` to build SW,
-    // manually overriding the skipWaiting();
-    self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
-    self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+	// });
 
-    self.addEventListener('message', function (event) {
-        if (event.data.action === 'skipWaiting') {
-            console.log("Skipping waiting")
-            self.skipWaiting();
-        }
-    });
+	// Manual injection point for manifest files.
+	// All assets under build/ and 5MB sizes are precached.
+	workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
+	// Font caching
+	workbox.routing.registerRoute(
+		new RegExp('https://fonts.(?:.googlepis|gstatic).com/(.*)'),
+		workbox.strategies.cacheFirst({
+			cacheName: 'googleapis',
+			plugins: [
+				new workbox.expiration.Plugin({
+					maxEntries: 30
+				})
+			]
+		})
+	);
 
-    // The event listener that is fired when the service worker updates
-    // Here we reload the page
-    // navigator.serviceWorker.addEventListener('controllerchange', function () {
+	// Image caching
+	workbox.routing.registerRoute(
+		/\.(?:png|gif|jpg|jpeg|svg)$/,
+		workbox.strategies.cacheFirst({
+			cacheName: 'images',
+			plugins: [
+				new workbox.expiration.Plugin({
+					maxEntries: 60,
+					maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+				})
+			]
+		})
+	);
 
-    // });
-
-    // Manual injection point for manifest files.
-    // All assets under build/ and 5MB sizes are precached.
-    workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
-
-    // Font caching
-    workbox.routing.registerRoute(
-        new RegExp("https://fonts.(?:.googlepis|gstatic).com/(.*)"),
-        workbox.strategies.cacheFirst({
-            cacheName: "googleapis",
-            plugins: [
-                new workbox.expiration.Plugin({
-                    maxEntries: 30
-                })
-            ]
-        })
-    );
-
-    // Image caching
-    workbox.routing.registerRoute(
-        /\.(?:png|gif|jpg|jpeg|svg)$/,
-        workbox.strategies.cacheFirst({
-            cacheName: "images",
-            plugins: [
-                new workbox.expiration.Plugin({
-                    maxEntries: 60,
-                    maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
-                })
-            ]
-        })
-    );
-
-    // JS, CSS caching
-    workbox.routing.registerRoute(
-        /\.(?:js|css)$/,
-        workbox.strategies.staleWhileRevalidate({
-            cacheName: "static-resources",
-            plugins: [
-                new workbox.expiration.Plugin({
-                    maxEntries: 60,
-                    maxAgeSeconds: 20 * 24 * 60 * 60 // 20 Days
-                })
-            ]
-        })
-    );
-
+	// JS, CSS caching
+	workbox.routing.registerRoute(
+		/\.(?:js|css)$/,
+		workbox.strategies.staleWhileRevalidate({
+			cacheName: 'static-resources',
+			plugins: [
+				new workbox.expiration.Plugin({
+					maxEntries: 60,
+					maxAgeSeconds: 20 * 24 * 60 * 60 // 20 Days
+				})
+			]
+		})
+	);
 } else {
-    console.error("Workbox could not be loaded. No offline support");
+	console.error('Workbox could not be loaded. No offline support');
 }
