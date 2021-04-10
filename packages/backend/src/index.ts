db657410d2c3e0217bca58fb2516fcaa20f3eb14
Martin Skalický <skalima7@fit.cvut.cz>
2021-04-10 16:49:08 +0200

    Remove old models

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -9,69 +9,68 @@ import loadersInit from "./loaders";
 import mongoose from "mongoose";
 import initPrivileges from "framework-ui/lib/privileges";
 import { groupsHeritage, allowedGroups } from "common/lib/constants/privileges";
-import "./models/Device"; // init
 
 export const routes = {
-	user: {
-		allowedGroups: allowedGroups.user,
-	},
-	admin: {
-		allowedGroups: allowedGroups.admin,
-	},
-	root: {
-		allowedGroups: allowedGroups.root,
-	},
+    user: {
+        allowedGroups: allowedGroups.user,
+    },
+    admin: {
+        allowedGroups: allowedGroups.admin,
+    },
+    root: {
+        allowedGroups: allowedGroups.root,
+    },
 };
 
 initPrivileges(routes, groupsHeritage);
 
 interface customApp extends Application {
-	server?: http.Server;
+    server?: http.Server;
 }
 
 async function startServer(config: Config) {
-	JwtService.init(config.jwt);
+    JwtService.init(config.jwt);
 
-	const app: customApp = express();
+    const app: customApp = express();
 
-	app.server = http.createServer(app);
-	if (!app.server) throw Error("Unable to create server");
+    app.server = http.createServer(app);
+    if (!app.server) throw Error("Unable to create server");
 
-	await loadersInit({ app, config });
+    await loadersInit({ app, config });
 
-	if (process.env.NODE_ENV_TEST !== "true") checkAndCreateRoot(); // check for roor existence, if not, then ask for password in terminal
+    if (process.env.NODE_ENV_TEST !== "true") checkAndCreateRoot(); // check for roor existence, if not, then ask for password in terminal
 
-	if (process.env.NODE_ENV === "development") {
-		console.log("Socket redirect enabled");
-		const proxy = require("http-proxy-middleware");
-		var wsProxy = proxy("/socket.io", {
-			target: "ws://localhost:8084",
-			changeOrigin: true, // for vhosted sites, changes host header to match to target's host
-			ws: true, // enable websocket proxy
-			logLevel: "error",
-		});
+    if (process.env.NODE_ENV === "development") {
+        console.log("Socket redirect enabled");
+        const proxy = require("http-proxy-middleware");
+        var wsProxy = proxy("/socket.io", {
+            target: "ws://localhost:8084",
+            changeOrigin: true, // for vhosted sites, changes host header to match to target's host
+            ws: true, // enable websocket proxy
+            logLevel: "error",
+        });
 
-		app.use(wsProxy);
-		app.server.on("upgrade", wsProxy.upgrade);
-		devLog("Proxy enabled");
-	}
+        app.use(wsProxy);
+        app.server.on("upgrade", wsProxy.upgrade);
+        devLog("Proxy enabled");
+    }
 
-	app.server.listen(config.port, () => {
-		console.log(`Started on port ${(app.server?.address() as any).port}`);
-	});
+    app.server.listen(config.port, () => {
+        console.log(`Started on port ${(app.server?.address() as any).port}`);
+    });
 
-	process.once("SIGINT", shutDown(app, mongoose.connection));
-	process.once("SIGTERM", shutDown(app, mongoose.connection));
-	process.once("SIGHUP", shutDown(app, mongoose.connection));
+    process.once("SIGINT", shutDown(app, mongoose.connection));
+    process.once("SIGTERM", shutDown(app, mongoose.connection));
+    process.once("SIGHUP", shutDown(app, mongoose.connection));
 }
 
 startServer(config);
 
 function shutDown(app: customApp, connection: mongoose.Connection) {
-	return async (code: NodeJS.Signals) => {
-		console.log(code, "received...");
-		app.server?.close();
-		await connection.close();
-		process.exit(0);
-	};
+    return async (code: NodeJS.Signals) => {
+        console.log(code, "received...");
+        app.server?.close();
+        await connection.close();
+        process.exit(0);
+    };
 }
--- packages/backend/src/types/index.ts
+++ packages/backend/src/types/index.ts
@@ -1,57 +1,63 @@
 import { ControlRecipe } from "common/lib/types";
+import type { Request } from "express"
+import { IUser } from "common/lib/models/interface/userInterface";
 
 export interface EmitterEvents {
-	user_login: any;
-	user_signup: UserBasic;
-	device_control_recipe_change: { recipes: ControlRecipe[]; deviceId: string };
-	device_delete: string;
-	device_create: string;
-	devices_delete: string[];
+    user_login: any;
+    user_signup: UserBasic;
+    device_control_recipe_change: { recipes: ControlRecipe[]; deviceId: string };
+    device_delete: string;
+    device_create: string;
+    devices_delete: string[];
 }
 
 export interface UserBasic {
-	id: string;
-	info: {
-		firstName: string;
-		email?: string;
-		lastName: string;
-		userName: string;
-	};
+    id: string;
+    info: {
+        firstName: string;
+        email?: string;
+        lastName: string;
+        userName: string;
+    };
 }
 
 export interface Config {
-	port: number;
-	bodyLimit: string;
-	homepage: string;
-	imagesPath: string;
-	portAuth: number;
-	db: {
-		userName: string;
-		password: string;
-		dbName: string;
-		url: string;
-		port: number;
-	};
-	jwt: {
-		privateKey: string;
-		publicKey: string;
-		expiresIn: string;
-	};
-	testUser: string;
-	testPassword: string;
-	email: {
-		host: string;
-		port: number;
-		secure: boolean;
-		userName: string;
-		password: string;
-	};
-	agenda: {
-		url: string;
-		port: number;
-		dbName: string;
-		userName: string;
-		password: string;
-		jobs?: string;
-	};
+    port: number;
+    bodyLimit: string;
+    homepage: string;
+    imagesPath: string;
+    portAuth: number;
+    db: {
+        userName: string;
+        password: string;
+        dbName: string;
+        url: string;
+        port: number;
+    };
+    jwt: {
+        privateKey: string;
+        publicKey: string;
+        expiresIn: string;
+    };
+    testUser: string;
+    testPassword: string;
+    email: {
+        host: string;
+        port: number;
+        secure: boolean;
+        userName: string;
+        password: string;
+    };
+    agenda: {
+        url: string;
+        port: number;
+        dbName: string;
+        userName: string;
+        password: string;
+        jobs?: string;
+    };
+}
+
+export type RequestWithAuth = Request & {
+    user: IUser
 }
