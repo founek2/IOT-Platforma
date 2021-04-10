547de6ca3632a07d33ee8bfd9d4e39d158175810
Martin Skalický <skalima7@fit.cvut.cz>
2021-04-10 21:35:21 +0200

    Remove permissions on user deletion

--- packages/common/src/services/userService.ts
+++ packages/common/src/services/userService.ts
@@ -7,77 +7,88 @@ import { IUserDocument } from "../models/schema/userSchema";
 import mongoose from "mongoose";
 import { AuthTypes } from "../constants";
 import { NotifyModel } from "../models/notifyModel";
+import { DeviceModel } from "../models/deviceModel";
 
 async function createHash(plainText: string) {
-	return argon2.hash(plainText);
+    return argon2.hash(plainText);
 }
 
 function comparePasswd(plainText: string, hash: IUser["auth"]["password"]) {
-	return argon2.verify(hash, plainText);
+    return argon2.verify(hash, plainText);
 }
 
 export type UserWithToken = { doc: IUser; token: string };
 export type CredentialData = {
-	userName: IUser["info"]["userName"];
-	password: IUser["auth"]["password"];
-	authType: IUser["auth"]["type"];
+    userName: IUser["info"]["userName"];
+    password: IUser["auth"]["password"];
+    authType: IUser["auth"]["type"];
 };
 
 export const UserService = {
-	async create(object: IUser): Promise<UserWithToken> {
-		const { password, type } = object.auth;
-		if (type && type !== AuthTypes.PASSWD) throw new Error("notImplemented");
-		devLog("user creating:", object);
-		const hash = await createHash(password);
+    async create(object: IUser): Promise<UserWithToken> {
+        const { password, type } = object.auth;
+        if (type && type !== AuthTypes.PASSWD) throw new Error("notImplemented");
+        devLog("user creating:", object);
+        const hash = await createHash(password);
 
-		const user = new UserModel({ ...object, auth: { password: hash }, realm: object.info.userName });
-		const obj = await user.save();
-		const plainUser = obj.toObject();
+        const user = new UserModel({ ...object, auth: { password: hash }, realm: object.info.userName });
+        const obj = await user.save();
+        const plainUser = obj.toObject();
 
-		const token = await JwtService.sign(plainUser);
-		return { doc: plainUser, token };
-	},
+        const token = await JwtService.sign(plainUser);
+        return { doc: plainUser, token };
+    },
 
-	async checkCreditals({ userName, authType, password }: CredentialData): Promise<UserWithToken> {
-		if (authType !== AuthTypes.PASSWD) throw new Error("notImplemented");
+    async checkCreditals({ userName, authType, password }: CredentialData): Promise<UserWithToken> {
+        if (authType !== AuthTypes.PASSWD) throw new Error("notImplemented");
 
-		const doc = await UserModel.findOne({ "info.userName": userName, "auth.type": authType });
-		if (!doc) throw Error("unknownUser");
+        const doc = await UserModel.findOne({ "info.userName": userName, "auth.type": authType });
+        if (!doc) throw Error("unknownUser");
 
-		const matched = await comparePasswd(password, doc.auth.password);
-		if (!matched) throw Error("passwordMissmatch");
+        const matched = await comparePasswd(password, doc.auth.password);
+        if (!matched) throw Error("passwordMissmatch");
 
-		const token = await JwtService.sign({ id: doc._id });
+        const token = await JwtService.sign({ id: doc._id });
 
-		return {
-			token,
-			doc: doc.toObject(),
-		};
-	},
+        return {
+            token,
+            doc: doc.toObject(),
+        };
+    },
 
-	async updateUser(userID: IUser["_id"], data: Partial<IUser>): Promise<IUserDocument> {
-		if (data.auth && data.auth.password) {
-			const { password } = data.auth;
-			const hash = await createHash(password);
-			data.auth.password = hash;
-		} else delete data.auth;
+    async updateUser(userID: IUser["_id"], data: Partial<IUser>): Promise<IUserDocument> {
+        if (data.auth && data.auth.password) {
+            const { password } = data.auth;
+            const hash = await createHash(password);
+            data.auth.password = hash;
+        } else delete data.auth;
 
-		const doc = await UserModel.findOneAndUpdate({ _id: mongoose.Types.ObjectId(userID) }, { $set: data });
-		if (!doc) throw Error("unknownUser");
+        const doc = await UserModel.findOneAndUpdate({ _id: mongoose.Types.ObjectId(userID) }, { $set: data });
+        if (!doc) throw Error("unknownUser");
 
-		return doc;
-	},
+        return doc;
+    },
 
-	async deleteById(userId: IUser["_id"]): Promise<boolean> {
-		const result = await UserModel.deleteOne({
-			_id: mongoose.Types.ObjectId(userId),
-		});
+    async deleteById(id: IUser["_id"]): Promise<boolean> {
+        const userId = mongoose.Types.ObjectId(id)
 
-		if (result.deletedCount !== 1) return false;
-		await NotifyModel.deleteMany({
-			userId: mongoose.Types.ObjectId(userId),
-		});
+        const result = await UserModel.deleteOne({
+            _id: userId
+        });
 
-		return true;
-	},
+        if (result.deletedCount !== 1) return false;
+        NotifyModel.deleteMany({
+            userId: userId
+        }).exec();
+
+        DeviceModel.updateMany({
+            $pull: {
+                "permissions.read": userId,
+                "permissions.control": userId,
+                "permissions.write": userId,
+            }
+        }).exec();
+
+        return true;
+    },
 };
