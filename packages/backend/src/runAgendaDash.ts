941626b9d0b315e983e7161b366c8f7ec9f25d11
Martin Skalický <skalicky.martas@seznam.cz>
2021-04-24 00:04:42 +0200

    FIX

--- packages/backend/src/runAgendaDash.ts
+++ packages/backend/src/runAgendaDash.ts
@@ -1,33 +1,33 @@
-import config from "common/lib/config";
-import { spawn } from "child_process";
-import createMongoUri from "common/lib/utils/createMongoUri";
-import path from "path";
+import config from 'common/lib/config';
+import { spawn } from 'child_process';
+import createMongoUri from 'common/lib/utils/createMongoUri';
+import path from 'path';
 
-const pathToBin = path.resolve(__dirname, "../node_modules/.bin/agendash");
+const pathToBin = path.resolve(__dirname, '../node_modules/.bin/agendash');
 
 let child: any;
-const argv = [`--db=${createMongoUri(config.agenda)}`, "--collection=agendaJobs", "--port=8089"];
+const argv = [`--db=${createMongoUri(config.db)}`, '--collection=agendaJobs', '--port=8089'];
 
 function startChild() {
-	console.log("STARTING", process.execPath, "child.js", argv);
-	child = spawn(pathToBin, argv, {
-		cwd: process.cwd(),
-		env: process.env,
-		detached: true,
-	});
-	child.on("error", function (e: any) {
-		console.log(e);
-	});
-	child.stdout.pipe(process.stdout);
-	console.log("STARTED with PID:", child.pid);
+    console.log('STARTING', process.execPath, 'child.js', argv);
+    child = spawn(pathToBin, argv, {
+        cwd: process.cwd(),
+        env: process.env,
+        detached: true,
+    });
+    child.on('error', function (e: any) {
+        console.log(e);
+    });
+    child.stdout.pipe(process.stdout);
+    console.log('STARTED with PID:', child.pid);
 }
 
 const callback = function () {
-	console.log("Quiting...");
-	child.kill();
-	process.exit();
+    console.log('Quiting...');
+    child.kill();
+    process.exit();
 };
 
-process.on("SIGQUIT", callback);
-process.on("SIGINT", callback);
+process.on('SIGQUIT', callback);
+process.on('SIGINT', callback);
 startChild();
