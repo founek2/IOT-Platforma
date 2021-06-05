08f8edb43dcac3fe233aa580cbfa5bbbe50cca41
Martin Skalický <skalicky.martas@seznam.cz>
2021-06-05 22:07:51 +0200

    Opt required in Descriptors

--- packages/framework-ui/src/types/index.ts
+++ packages/framework-ui/src/types/index.ts
@@ -6,7 +6,7 @@ export type messages = { [messageKey: string]: ((args: any) => string) | string 
 
 export type fieldDescriptor = {
     deepPath: string;
-    required: boolean;
+    required?: boolean;
     when?: ((formData: any, options: { deepPath: string; i?: number }) => boolean) | null;
     getLength?: ((formData: any) => number | undefined) | null | undefined; // just when deepPath ends with [] -> will be array of fields
     label?: string | null | undefined;
