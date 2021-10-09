be76f466d542e2cc330feac7cf7d2f53e0d33a2d
Martin Skalický <skalima7@fit.cvut.cz>
2021-10-09 13:05:09 +0200

    Remove invalid arg

--- ci/jenkins/release.sh
+++ ci/jenkins/release.sh
@@ -2,7 +2,7 @@
 set -e
 
 # pack all release files
-zip -r build.zip packages/backend/{dist,package.json} packages/backend-mqtt/{dist,package.json} packages/common/{lib,package.json} packages/framework-ui/{lib,package.json} packages/frontend/build package.json license.md process.json
+zip -qr assets.zip packages/backend/{dist,package.json} packages/backend-mqtt/{dist,package.json} packages/common/{lib,package.json} packages/framework-ui/{lib,package.json} packages/frontend/build package.json license.md process.json
 
 # Build
 # Publish on github
@@ -15,7 +15,7 @@ message="$(git for-each-ref refs/tags/$tag --format='%(contents)')"
 # Get the title and the description as separated variables
 name=$(echo "$message" | head -n1)
 description=$(echo "$message" | tail -n +3)
-description=$(echo "$description" | sed -z 's/\n/\\n/g') # Escape line breaks to prevent json parsing problems
+description=$(echo "$description" | sed 's/\n/\\n/g') # Escape line breaks to prevent json parsing problems
 
 # Create a release
 echo "Creating releasing with tag_name=$tag"
@@ -23,4 +23,4 @@ release=$(curl -XPOST -H "Authorization: token $token" -H "Accept: application/v
 # Extract the id of the release from the creation response
 id=$(echo "$release" | sed -n -e 's/"id":\ \([0-9]\+\),/\1/p' | head -n 1 | sed 's/[[:blank:]]//g')
 # Upload the artifact
-curl -XPOST -H "Authorization: token $token" -H "Content-Type:application/octet-stream" -H "Accept: application/vnd.github.v3+json" --data-binary @build.zip https://uploads.github.com/repos/founek2/IOT-Platforma/releases/$id/assets?name=assets.zip
+curl -XPOST -H "Authorization: token $token" -H "Content-Type:application/octet-stream" -H "Accept: application/vnd.github.v3+json" --data-binary @assets.zip https://uploads.github.com/repos/founek2/IOT-Platforma/releases/$id/assets?name=assets.zip
