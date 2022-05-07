2e4e931c4bf11712eb2ad40937a52408e5c4d623
Martin Skalický <skalicky.martas@seznam.cz>
2022-05-07 18:34:41 +0200

    Publish to private registry

--- ci/jenkins/docker-publish.sh
+++ ci/jenkins/docker-publish.sh
@@ -9,3 +9,8 @@ docker build -t $name:$tag -t $name:latest .
 docker login -u "$USER_CREDENTIALS_USR" -p "$USER_CREDENTIALS_PSW"
 docker push $name:latest
 docker push $name:$tag
+
+name_private="docker-registry.iotdomu.cz/iot-platform/server:latest"
+docker build -t $name_private .
+
+docker push $name_private
