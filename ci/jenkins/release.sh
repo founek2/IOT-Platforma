24b5ea932f103b35929a00ded005a248be4dfdeb
Martin Skalický <skalicky.martas@seznam.cz>
2022-06-14 00:03:52 +0200

    Fix add auth to release zip

--- ci/jenkins/release.sh
+++ ci/jenkins/release.sh
@@ -2,7 +2,7 @@
 set -e
 
 # pack all release files
-zip -qr assets.zip packages/backend/{dist,package.json} packages/backend-mqtt/{dist,package.json} packages/common/{lib,package.json} packages/framework-ui/{lib,package.json} packages/frontend/build package.json license.md process.json yarn.lock
+zip -qr assets.zip packages/backend/{dist,package.json} packages/backend-mqtt/{dist,package.json} packages/backend-auth/{dist,package.json}  packages/common/{lib,package.json} packages/framework-ui/{lib,package.json} packages/frontend/build package.json license.md process.json yarn.lock
 
 # Build
 # Publish on github
