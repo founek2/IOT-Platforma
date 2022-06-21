a5d4cbb041750b390c9f30bc7639442065a6d044
Martin Skalický <skalicky.martas@seznam.cz>
2022-06-21 22:17:52 +0200

    SW cache first JS chunk

--- packages/frontend/src/service-worker.ts
+++ packages/frontend/src/service-worker.ts
@@ -12,7 +12,7 @@ import { clientsClaim } from 'workbox-core';
 import { ExpirationPlugin } from 'workbox-expiration';
 import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
-import { StaleWhileRevalidate } from 'workbox-strategies';
+import { StaleWhileRevalidate, CacheFirst } from 'workbox-strategies';
 
 declare const self: ServiceWorkerGlobalScope;
 
@@ -75,7 +75,7 @@ registerRoute(
     // Add in any other file extensions or routing criteria as needed.
     ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.js'),
     // Customize this strategy as needed, e.g., by changing to CacheFirst.
-    new StaleWhileRevalidate({
+    new CacheFirst({
         cacheName: 'js-assets',
         plugins: [
             // Ensure that once this runtime cache reaches a maximum size the
