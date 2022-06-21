201a5edddb1879466f1f28422dcffa3c0914bd9c
Martin Skalický <skalicky.martas@seznam.cz>
2022-06-21 22:58:44 +0200

    Remove bundle before zipping

--- ci/jenkins/bundle-zip.sh
+++ ci/jenkins/bundle-zip.sh
@@ -2,4 +2,5 @@
 set -e
 
 # pack all release files
+rm -f assets.zip
 zip -qr assets.zip packages/backend/{dist,package.json} packages/backend-mqtt/{dist,package.json} packages/backend-auth/{dist,package.json}  packages/common/{lib,package.json} packages/framework-ui/{lib,package.json} packages/frontend/build package.json license.md process.json yarn.lock
