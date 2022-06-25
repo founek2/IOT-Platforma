57293e94b2a839aafd16657f463ef83d7f21b019
Martin Skalický <skalicky.martas@seznam.cz>
2022-06-25 23:14:15 +0200

    Fix path recovery

--- packages/frontend/src/containers/CustomRouter.tsx
+++ packages/frontend/src/containers/CustomRouter.tsx
@@ -1,16 +1,12 @@
 import { historyActions } from 'framework-ui/src/redux/actions/history';
 import { historyReducerActions } from 'framework-ui/src/redux/reducers/history';
 import parseQuery from 'framework-ui/src/utils/parseQuery';
-import { BrowserHistory, createBrowserHistory, History } from 'history';
+import { BrowserHistory } from 'history';
 import React, { ReactNode } from 'react';
-import { useDispatch } from 'react-redux';
 import { Router } from 'react-router-dom';
 import { useAppDispatch } from 'src/hooks';
 import '../firebase'; // init
 
-const history = createBrowserHistory();
-
-const defLocation = history.location;
 interface CustomRouterProps {
     children: ReactNode;
     history: BrowserHistory;
@@ -24,18 +20,18 @@ export function CustomRouter({ children, history }: CustomRouterProps) {
     });
 
     React.useLayoutEffect(() => {
+        const lastHistory = localStorage.getItem('history');
+        const targetLocation =
+            lastHistory && history.location.pathname === '/' ? JSON.parse(lastHistory) : history.location;
+
         dispatch(
             historyActions.set({
-                pathname: defLocation.pathname,
-                hash: defLocation.hash,
-                search: defLocation.search,
-                query: parseQuery(defLocation.search),
+                pathname: targetLocation.pathname,
+                hash: targetLocation.hash,
+                search: targetLocation.search,
+                query: parseQuery(targetLocation.search),
             })
         );
-
-        const lastHistory = localStorage.getItem('history');
-
-        if (lastHistory && history.location.pathname === '/') history.push(JSON.parse(lastHistory));
 
         history.listen((update) => {
             const { key, state, ...rest } = update.location;
@@ -47,20 +43,12 @@ export function CustomRouter({ children, history }: CustomRouterProps) {
             );
 
             setState(update);
-
-            // updateTmpDataAction({ dialog: {} });
-
             localStorage.setItem('history', JSON.stringify(rest));
         });
+
+        // Sync history with redux
+        history.push(targetLocation);
     }, [history, dispatch]);
 
-    return (
-        <Router
-            basename="/"
-            children={children}
-            location={state.location}
-            navigationType={state.action}
-            navigator={history}
-        />
-    );
+    return <Router basename="/" children={children} location={state.location} navigator={history} />;
 }
