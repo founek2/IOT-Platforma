c90cdede131bab284a2a8289214c4b6c0c52923b
Martin Skalický <skalicky.martas@seznam.cz>
2022-06-25 10:51:58 +0200

    Fix prod build

--- ci/jenkins/docker-publish.sh
+++ ci/jenkins/docker-publish.sh
@@ -5,6 +5,8 @@ tag=$(git describe --abbrev=0)
 name=founek2/iot-platform
 name_private="docker-registry.iotdomu.cz/iot-platform/server"
 
+wget https://github.com/founek2/IOT-Platforma/releases/latest/download/assets.zip -O assets.zip
+
 docker build -t $name:$tag -t $name:latest -t $name_private:$tag -t $name_private:latest --progress plain .
 
 docker push $name:latest
