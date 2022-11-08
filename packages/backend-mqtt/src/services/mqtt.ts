58c4cfbbad6d77433c250b89375e925ba8ca8de2
Martin Skalický <skalicky.martas@seznam.cz>
2022-11-08 16:33:25 +0100

    Reconnect on close connection

--- packages/backend-mqtt/src/services/mqtt.ts
+++ packages/backend-mqtt/src/services/mqtt.ts
@@ -59,6 +59,7 @@ async function reconnect(config: MqttConf, getUser: GetUser, cb: ClientCb): Prom
             reconnectPeriod: 0,
             port: config.port,
             rejectUnauthorized: false,
+            protocolVersion: 3,
         });
         cb(client);
         return client;
@@ -96,6 +97,7 @@ function applyListeners(io: serverIO, cl: MqttClient, config: MqttConf, getUser:
 
     cl.on('close', async function () {
         logger.info('mqtt closed connection');
+        client = await connect(config, getUser, (cl) => applyListeners(io, cl, config, getUser));
     });
     cl.on('disconnect', async function () {
         logger.info('mqtt disconnected');
