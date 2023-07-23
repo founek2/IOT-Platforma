3ce3d26cf7306677abd85e7cd505d552b7a4eb1c
Martin Skalický <skalicky.martas@seznam.cz>
2023-07-23 14:05:06 +0200

    Use original historical Schema

--- packages/common/src/models/schema/historicalSchema.ts
+++ packages/common/src/models/schema/historicalSchema.ts
@@ -4,44 +4,37 @@ import { HistoricalSensor } from '../interface/history';
 const Schema = mongoose.Schema;
 const ObjectId = mongoose.Types.ObjectId;
 
-export interface IHistorical extends HistoricalSensor, Document {}
-
-// export const historicalSchemaPlain = {
-//     deviceId: ObjectId,
-//     thingId: ObjectId,
-//     day: Date,
-//     first: Date,
-//     last: Date,
-//     properties: {
-//         type: Map,
-//         of: new Schema({
-//             max: Number,
-//             min: Number,
-//             nsamples: {
-//                 night: Number,
-//                 Day: Number,
-//             },
-//             samples: [{ value: Schema.Types.Mixed, timestamp: Date }],
-//             sum: {
-//                 night: Number,
-//                 Day: Number,
-//             },
-//         }),
-//     },
-//     nsamples: Number,
-// };
-
-// export const historicalSchemaPlain = {
-//     deviceId: ObjectId,
-//     thingId: ObjectId,
-//     propertyId: ObjectId,
-//     value: Schema.Types.Mixed,
-//     timestamp: Date,
-// };
+export interface IHistorical extends HistoricalSensor, Document { }
 
 export const historicalSchemaPlain = {
     deviceId: ObjectId,
     thingId: ObjectId,
-    propertyId: ObjectId,
-    data: { value: Schema.Types.Mixed, timestamp: Date },
+    day: Date,
+    first: Date,
+    last: Date,
+    properties: {
+        type: Map,
+        of: new Schema({
+            max: Number,
+            min: Number,
+            nsamples: {
+                night: Number,
+                Day: Number,
+            },
+            samples: [{ value: Schema.Types.Mixed, timestamp: Date }],
+            sum: {
+                night: Number,
+                Day: Number,
+            },
+        }),
+    },
+    nsamples: Number,
 };
+
+/// This is how it might look, to make it more flat
+// export const historicalSchemaPlain = {
+//     deviceId: ObjectId,
+//     thingId: ObjectId,
+//     propertyId: ObjectId,
+//     data: { value: Schema.Types.Mixed, timestamp: Date },
+// };
