876fbfb1ec6d5efdce4139313137d83fd7fdea5e
Martin Skalický <skalicky.martas@seznam.cz>
2023-08-20 17:58:50 +0200

    Fix image build

--- ci/jenkins/docker-build.sh
+++ ci/jenkins/docker-build.sh
@@ -6,5 +6,9 @@ unzip -q assets.zip
 # backport compatibility for react-scripts v3
 cp packages/frontend/build/service-worker.js packages/frontend/build/sw.js
 
+# Remove all occurences of linked packages by workspace: syntax
+sed -i'' '/workspace:/d' packages/*/package.json
+sed -i'' '/"packageManager":/d' package.json
+
 yarn install --production --cache-folder /tmp/.junk
 rm -rf /tmp/.junk
