e9a9d8d7201fcef85cec0d1e170bf212cfb89009
Martin Skalický <skalicky.martas@seznam.cz>
2024-02-15 19:34:39 +0100

    Do data preprocessing on FE for Dashboard

--- modules/frontend/src/Pages/Dashboard.tsx
+++ modules/frontend/src/Pages/Dashboard.tsx
@@ -2,59 +2,45 @@ import { Card, CardContent, CardHeader, CircularProgress, Grid } from "@mui/mate
 import React from "react"
 import DataList from "../components/DataList";
 import PlotifyGauge from "../components/PlotifyGauge";
-import { BrokerConnectionItem, BrokerData, useBrokerQuery } from "../endpoints/broker"
+import { BrokerConnectionItem, BrokerData, ConnectionType, ItemExtended, useBrokerQuery } from "../endpoints/broker"
 import { useDevicesAllQuery, useDevicesQuery } from "../endpoints/devices";
 
-
-enum ConnectionType {
-    user, device, guest
-}
-type ItemExtended = BrokerConnectionItem & { _id: string, type: ConnectionType }
-
-function detectConnectionType(item: BrokerConnectionItem) {
-    const user = item.user;
-    if (user.startsWith("guest="))
-        return ConnectionType.guest;
-    if (user.startsWith("device="))
-        return ConnectionType.device;
-
-    return ConnectionType.user;
-}
 
 function extractHumanText(item: ItemExtended) {
     const user = item.user;
     const userSplitted = user.split("=");
 
+    let text: string
     if (item.type == ConnectionType.guest)
-        return `Čeká na připojení ${userSplitted[1]}`
-    if (item.type == ConnectionType.device)
-        return `Zařízení ${userSplitted[1]}`
-    if (item.type == ConnectionType.user)
-        return `Uživatel ${user}`
+        text = `Čeká na připojení ${userSplitted[1]}`
+    else if (item.type == ConnectionType.device)
+        text = `Zařízení ${userSplitted[1]}`
+    else if (item.type == ConnectionType.user)
+        text = `Uživatel ${user}`
+    else
+        text = `Neznámé spojení`
 
-    return `Neznámé spojení`
+    if (item.connections.length > 1)
+        return `${text} (${item.connections.length})`
+    else return text
 }
 
-function sortByName(a: BrokerConnectionItem, b: BrokerConnectionItem,) {
-    return a.user.localeCompare(b.user)
-}
+
 
 export default function Dashboard() {
     const { data, isLoading } = useBrokerQuery()
     const { data: devices, isLoading: isLoadingDevices } = useDevicesAllQuery()
 
     if (isLoading || isLoadingDevices) return <CircularProgress />;
-
-    const dataWithId = data?.connections.items.map(item => ({ _id: item.name, ...item, type: detectConnectionType(item) }));
-    dataWithId?.sort(sortByName)
-    const dataPaired = dataWithId?.filter(item => item.type == ConnectionType.device);
+    const dataConnections = data?.connections.items;
+    const dataPaired = dataConnections?.filter(item => item.type == ConnectionType.device);
 
     return <Grid container justifyContent="center" spacing={2}>
         <Grid item xs={12} md={7} lg={6} xl={3}>
             <Card sx={{ padding: 2 }}>
                 <CardHeader title="Aktivní spojení" />
                 <CardContent>
-                    <DataList data={dataWithId || []} getHumanText={extractHumanText} />
+                    <DataList data={dataConnections || []} getHumanText={extractHumanText} />
                 </CardContent>
             </Card>
         </Grid>
--- modules/frontend/src/endpoints/broker.ts
+++ modules/frontend/src/endpoints/broker.ts
@@ -6,24 +6,67 @@ export type BrokerConnectionItem = {
     user: string
     node: string
 };
-export type BrokerData = {
+export type BrokerData<T = BrokerConnectionItem> = {
     overview: any,
     connections: {
         total_count: number
         item_count: number
-        items: BrokerConnectionItem[]
+        items: T[]
     }
     updatedAt: string
 }
 
+export enum ConnectionType {
+    user, device, guest
+}
+export type ItemExtended = BrokerConnectionItem & { _id: string, type: ConnectionType, connections: { name: string }[] }
+
+function detectConnectionType(item: BrokerConnectionItem) {
+    const user = item.user;
+    if (user.startsWith("guest="))
+        return ConnectionType.guest;
+    if (user.startsWith("device="))
+        return ConnectionType.device;
+
+    return ConnectionType.user;
+}
+
+
 export const brokerApi = api.injectEndpoints({
     endpoints: (build) => ({
-        broker: build.query<BrokerData, void>({
+        broker: build.query<BrokerData<ItemExtended>, void>({
             query: () => `broker`,
             providesTags: ['Broker'],
-            transformResponse: (body: any) => body.data,
+            transformResponse: (body: { data: BrokerData }): BrokerData<ItemExtended> => {
+                const items = body.data.connections.items;
+                const data: Record<string, ItemExtended> = {};
+                items.forEach(item => {
+                    if (data[item.user]) {
+                        data[item.user].connections.push({ name: item.name })
+                    } else {
+                        data[item.user] = { ...item, _id: item.user, connections: [{ name: item.name }], type: detectConnectionType(item) }
+                    }
+                })
+                const dataWithId = Object.values(data);
+
+                function sortByName(a: BrokerConnectionItem, b: BrokerConnectionItem,) {
+                    return a.user.localeCompare(b.user)
+                }
+                dataWithId?.sort(sortByName)
+
+                return {
+                    overview: body.data.overview,
+                    connections: {
+                        ...body.data.connections,
+                        items: dataWithId
+                    },
+                    updatedAt: body.data.updatedAt
+                };
+            }
         }),
     }),
 });
+
+
 
 export const { useBrokerQuery } = brokerApi;
